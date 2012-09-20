4e7791a03da7814c35fd4242842552f7c32f1569
Hugh Kennedy <hughskennedy@gmail.com>
2012-09-21 01:37:03 +1000

    First commit

---
+++ reductionist.js
@@ -0,0 +1,87 @@
+var map = require('map-stream')
+
+module.exports = function(memo, reduce, options) {
+    var options = options || {}
+      , stream
+      , queue = []
+      , ended = false
+      , oldend
+
+    if (typeof options.every === 'undefined') {
+        options.every = true
+    }
+
+    stream = map(function(chunk, callback) {
+
+        function next(err, result) {
+            queue.shift();
+
+            if (queue.length) {
+                reduce(stream.memo, queue.slice(0, 1)[0], reduction);    
+            }
+            
+            return arguments.length ? callback(err, result) : callback();
+        };
+
+        function reduction(err, result) {
+            if (err) {
+                return next(err)
+            }
+            if (typeof result === 'undefined') {
+                return next()
+            }
+
+            stream.memo = result
+
+            if (options.every) {
+                return next(null, result)
+            }
+
+            return next()
+        };
+
+        queue.push(chunk)
+
+        if (queue.length < 2) {
+            reduce(stream.memo, chunk, reduction)
+        }
+    })
+
+    stream.memo = memo || 0
+
+    oldend = stream.end
+    stream.end = function(data) {
+        if (ended) return
+        ended = true;
+
+        this.writable = false
+
+        if (data !== undefined) {
+            options.every = true
+            this.write(data);
+
+            return process.nextTick(function() {
+                stream.emit('end');
+                return process.nextTick(function() {
+                    stream.emit('close');
+                })
+            });
+        }
+
+        if (!options.every) {
+            this.emit('data', stream.memo)
+        }
+
+        process.nextTick(function() {
+            return oldend.call(stream)
+        });
+    };
+
+    stream.once('error', function() {
+        stream.removeAllListeners();
+        stream.write = function(){};
+        stream.queue = [];
+    });
+
+    return stream
+};
---
+++ test/index.js
@@ -0,0 +1,140 @@
+var reduce = require('../reductionist')
+  , assert = require('assert')
+  , tester = require('stream-tester')
+  , spec = require('stream-spec')
+
+function countToTen(stream) {
+    [1,2,3,4,5,6,7,8,9].forEach(function(n) {
+        stream.write(n);
+    })
+    stream.end(10);
+};
+
+suite('Always', function() {
+    test('Calling next() without arguments ignores that chunk', function(done) {
+        var stream
+          , output = []
+
+        stream = reduce(0, function(memo, num, next) {
+            if (num % 2) return next()
+            next(null, memo + num);
+        }).on('data', function(data) {
+            output.push(data);
+        }).once('end', function() {
+            assert.deepEqual(output, [
+                2, 6, 12, 20, 30
+            ]);
+            done();
+        });
+
+        countToTen(stream);
+    });
+
+    test('Passing an error to next() emits an error on that stream, shutting it down', function(done) {
+        var stream
+          , output = []
+
+        stream = reduce(0, function(memo, num, next) {
+            next(new Error('This should emit'))
+        }).on('error', function(err) {
+            assert.ok(err && err instanceof Error);
+            done();
+        })
+
+        spec(stream)
+            .through({ strict: true, error: true })
+
+        countToTen(stream);
+    });
+});
+
+suite('Stream Checks', function() {
+    test('Buffers on pause', function(done) {
+        var stream = reduce(0, function(memo, num, next) {
+            return next(null, memo + num)
+        })
+
+        spec(stream)
+            .through({
+                  strict: true
+                , error: false
+            })
+            .validateOnExit()
+
+        tester.createRandomStream(function() {
+            return Math.random() * 10
+        }, 1000)
+            .pipe(stream)
+            .pipe(tester.createPauseStream())
+            .on('end', done);
+    });
+});
+
+suite('options.every == false', function() {
+    test('Each change is emitted, including "end"', function(done) {
+        var stream
+          , output = []
+
+        stream = reduce(0, function(memo, num, next) {
+            next(null, memo + num)
+        }).on('data', function(n) {
+            output.push(n);
+        }).once('end', function() {
+            assert.deepEqual(output, [
+                1, 3, 6, 10, 15, 21, 28, 36, 45, 55
+            ]);
+            done();
+        });
+
+        countToTen(stream);
+    });
+
+    test('Changes remain in the order they were written', function(done) {
+        var stream
+          , output = []
+
+        stream = reduce(0, function(memo, num, next) {
+            setTimeout(function(){
+                next(null, memo + num)
+            }, 20 - num * 2);
+        }).on('data', function(n) {
+            output.push(n);
+
+            if (output.length === 9) {
+                return ready();
+            }
+        });
+
+        [1,2,3,4,5,6,7,8,9].forEach(function(n) {
+            stream.write(n);
+        });
+
+        function ready() {
+            assert.deepEqual(output, [
+                1, 3, 6, 10, 15, 21, 28, 36, 45
+            ]);
+            done();
+        };
+    });
+});
+
+suite('options.every == true', function() {
+    test('Only returns the final value', function(done){
+        var stream
+          , output = false
+
+        stream = reduce(0, function(memo, num, next) {
+            next(null, memo + num);
+        }, { every: false }).on('data', function(data) {
+            assert.equal(data, 55);
+            assert.equal(output, false);
+
+            output = true;
+        }).once('end', function() {
+            assert.equal(output, true);
+            done();
+        });
+
+        countToTen(stream);
+    });
+});
